1d43fb5bc469e49d36e9c4697dacd7e59450090c
Faintful <72162133+Faintful@users.noreply.github.com>
2023-11-17 11:52:18 +0100

    Esercizio TailwindCSS framework

---
+++ css/di_meglio/tailwind/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['C:UsersSkyeVSCProjectscorso_frontcssdi_meglio\tailwindindex.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
